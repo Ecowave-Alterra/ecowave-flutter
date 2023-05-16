a9c155b5b163f32b96bb482d441bbfc20057c3db
Hanief Dafi Satria <96201555+dafisatria@users.noreply.github.com>
2023-05-16 15:37:36 +0700

    added constant (#3)
    * added constant
    
    * add constant resolve
    
    * added constants
    
    * add core

--- lib/core.dart
+++ lib/core.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+part 'core/constants/colors.dart';
+part 'core/constants/font_weight.dart';
+part 'core/constants/icons.dart';
+part 'core/constants/images.dart';
+part 'core/constants/sizes.dart';
 
---
+++ lib/core/constants/sizes.dart
@@ -0,0 +1,6 @@
+part of 'package:ecowave/core.dart';
+
+class AppSizes {
+  static const primary = 16.0;
+  static const radius = 12.0;
+}
