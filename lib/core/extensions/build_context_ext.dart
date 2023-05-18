1c01b4d01558a12de942549b6b2a55fa91a0083c
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-05-19 06:35:34 +0700

    add extension for pushAndRemoveUntil

--- lib/core/extensions/build_context_ext.dart
+++ lib/core/extensions/build_context_ext.dart
@@ -67,14 +67,11 @@ extension NavigatorExt on BuildContext {
     );
   }
 
-  Future<T?> pushReplacement<T extends Object, TO extends Object>(Widget widget,
-      [String? name]) async {
+  Future<T?> pushReplacement<T extends Object, TO extends Object>(
+      Widget widget) async {
     return Navigator.pushReplacement<T, TO>(
       this,
-      MaterialPageRoute(
-        builder: (context) => widget,
-        settings: RouteSettings(name: name),
-      ),
+      MaterialPageRoute(builder: (context) => widget),
     );
   }
 
@@ -96,6 +93,15 @@ extension NavigatorExt on BuildContext {
       routeName,
       predicate,
       arguments: args,
+    );
+  }
+
+  Future<T?> pushAndRemoveUntil<T extends Object>(
+      Widget widget, bool Function(Route<dynamic> route) predicate) async {
+    return Navigator.pushAndRemoveUntil<T>(
+      this,
+      MaterialPageRoute(builder: (context) => widget),
+      predicate,
     );
   }
 }
