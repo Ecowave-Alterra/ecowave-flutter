040f6bc778f417cb7e13232c915cbe5ba3274379
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-05-19 09:05:39 +0700

    complete slicing payments

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:ecowave/core.dart';
+import 'package:ecowave/core/utils/providers.dart';
 import 'package:ecowave/features/payment/view/pages/payment_detail_page.dart';
 import 'package:ecowave/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
@@ -9,34 +11,37 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ecowave',
-      home: const PaymentDetailPage(),
-      onGenerateRoute: Routes.generateRoute,
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.white,
-        useMaterial3: true,
-        textTheme: GoogleFonts.poppinsTextTheme(
-          Theme.of(context).textTheme,
-        ).apply(
-          bodyColor: AppColors.black,
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: AppColors.white,
-          foregroundColor: AppColors.black,
-          centerTitle: true,
-          iconTheme: const IconThemeData(
-            color: AppColors.primary500,
+    return MultiBlocProvider(
+      providers: Providers.init,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ecowave',
+        home: const PaymentDetailPage(),
+        onGenerateRoute: Routes.generateRoute,
+        theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.white,
+          useMaterial3: true,
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ).apply(
+            bodyColor: AppColors.black,
           ),
-          shape: Border(
-            bottom: BorderSide(
-              color: AppColors.grey300.withOpacity(0.5),
+          appBarTheme: AppBarTheme(
+            backgroundColor: AppColors.white,
+            foregroundColor: AppColors.black,
+            centerTitle: true,
+            iconTheme: const IconThemeData(
+              color: AppColors.primary500,
+            ),
+            shape: Border(
+              bottom: BorderSide(
+                color: AppColors.grey300.withOpacity(0.5),
+              ),
             ),
           ),
-        ),
-        dividerTheme: DividerThemeData(
-          color: AppColors.grey300.withOpacity(0.5),
+          dividerTheme: DividerThemeData(
+            color: AppColors.grey300.withOpacity(0.5),
+          ),
         ),
       ),
     );
