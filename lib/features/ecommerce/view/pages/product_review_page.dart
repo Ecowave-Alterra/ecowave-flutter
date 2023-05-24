8b94bc94c2763e068d3ded317f05ed71d51ad9a7
Kev9794 <k.natanael88@gmail.com>
2023-05-25 00:50:34 +0700

    slicing correction

--- lib/features/ecommerce/view/pages/product_review_page.dart
+++ lib/features/ecommerce/view/pages/product_review_page.dart
@@ -1,6 +1,8 @@
 import 'package:ecowave/features/ecommerce/view/widgets/review_list_widget.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core.dart';
+
 class ProductReview extends StatelessWidget {
   const ProductReview({super.key});
 
@@ -10,11 +12,44 @@ class ProductReview extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Ulasan"),
       ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ReviewListWidget(),
+          Container(
+            padding: const EdgeInsets.all(AppSizes.primary),
+            child: Row(
+              children: [
+                const ImageIcon(
+                  AppIcons.solidUlasan,
+                  color: AppColors.warning500,
+                  size: 14.24,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: const Text(
+                    '4.5',
+                    style: TextStyle(
+                        fontSize: AppSizes.primary,
+                        fontWeight: AppFontWeight.semibold,
+                        color: AppColors.black),
+                  ),
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.only(left: 8.0, top: 2.0, bottom: 2.0),
+                  child: const Text(
+                    '2 penilaian',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: AppFontWeight.medium,
+                        color: AppColors.grey700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const ReviewListWidget(),
         ],
       ),
     );
