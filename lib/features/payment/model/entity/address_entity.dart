1d531aff67d87532f811713d477e33ef37528415
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-05-24 19:24:34 +0700

    [FL-012] Pilih Alamat Pengiriman (#17)
    * fixing problems
    
    * complete slicing change shipping address

--- lib/features/payment/model/entity/address_entity.dart
+++ lib/features/payment/model/entity/address_entity.dart
@@ -4,6 +4,7 @@ class AddressEntity {
   final String address;
   final String markedAs;
   final String? note;
+  final bool isMain;
 
   AddressEntity({
     required this.name,
@@ -11,5 +12,6 @@ class AddressEntity {
     required this.address,
     required this.markedAs,
     this.note,
+    this.isMain = false,
   });
 }
