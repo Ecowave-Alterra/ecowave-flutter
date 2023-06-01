31bb04e85c66ea22ef1892b4f924b3471a4ce266
Hanief Dafi Satria <96201555+dafisatria@users.noreply.github.com>
2023-06-01 12:49:50 +0700

    [FL-006] fixing appbar (#38)

--- lib/features/information/view/pages/bookmark_page.dart
+++ lib/features/information/view/pages/bookmark_page.dart
@@ -13,29 +13,12 @@ class BookmarkPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {
-              context.pop();
-            },
-            icon: const ImageIcon(AppIcons.leftBack,
-                color: AppColors.primary500, size: 11),
-          ),
           title: Text(
             'Bookmark',
             style: GoogleFonts.poppins(
               fontSize: 30,
               fontWeight: AppFontWeight.regular,
               color: AppColors.black,
-            ),
-          ),
-          backgroundColor: AppColors.white,
-          elevation: 0.0,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(2.0),
-            child: Container(
-              color: AppColors.grey300,
-              height: 2.0,
             ),
           ),
         ),
