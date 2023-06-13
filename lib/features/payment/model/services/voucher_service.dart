d2b64df95d9849471cbd8dfc7ca9ba8d0a21ba87
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-06-13 14:46:55 +0700

    success get voucher using mock api

--- lib/features/payment/model/services/voucher_service.dart
+++ lib/features/payment/model/services/voucher_service.dart
@@ -1,55 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:ecowave/core.dart';
 import 'package:ecowave/features/payment/model/models/voucher_model.dart';
 
 class VoucherService {
-  // late Dio _dio;
-  // VoucherRemoteDatasourceImpl() {
-  //   _dio = Dio();
-  // }
-
-  Map<String, dynamic> dummy = {
-    "Vouchers": [
-      {
-        "Id": 1,
-        "Type": "Diskon Belanja",
-        "MinimumPurchase": 50000,
-        "MaximumDiscount": 15000,
-        "DiscountPercent": 20,
-        "MaxClaimLimit": 2,
-        "ClaimableCount": 1000,
-        "StartDate": "2023-05-30T11:00:00Z",
-        "EndDate": "2023-06-30T11:00:00Z"
-      },
-      {
-        "Id": 2,
-        "Type": "Gratis Ongkir",
-        "ClaimableCount": 1000,
-        "MaxClaimLimit": 2,
-        "StartDate": "2023-05-30T11:00:00Z",
-        "EndDate": "2023-06-30T11:00:00Z"
-      },
-      {
-        "Id": 3,
-        "Type": "Gratis Ongkir",
-        "MinimumPurchase": 100000,
-        "ClaimableCount": 1000,
-        "MaxClaimLimit": 2,
-        "StartDate": "2023-05-30T11:00:00Z",
-        "EndDate": "2023-06-30T11:00:00Z"
-      }
-    ],
-    "Page": 1,
-    "Status": 200,
-    "TotalPage": 1
-  };
+  late Dio _dio;
+  VoucherService() {
+    _dio = Dio();
+  }
 
   Future<List<VoucherModel>> getVouchers() async {
     try {
-      // const String url = '${BaseURL.api}user/payment/vouchers';
-      // final response = await _dio.get(url);
+      const String url = '${BaseURL.mock}user/payment/vouchers';
+      final response = await _dio.get(url);
 
-      if (dummy["Status"] == 200) {
-        final List<Map<String, dynamic>> datas = dummy["Vouchers"];
-        return datas.map((e) => VoucherModel.fromJson(e)).toList();
+      if (response.statusCode == 200) {
+        final List datas = response.data["Vouchers"];
+        return datas
+            .map((e) => VoucherModel.fromJson(e as Map<String, dynamic>))
+            .toList();
       } else {
         throw "get vouchers not successfully";
       }
