69566306e5f61b81451e7ae2c8021320ef0a452b
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-06-19 07:01:21 +0700

    delete route directory

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:ecowave/core.dart';
 import 'package:ecowave/core/utils/providers.dart';
-import 'package:ecowave/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,7 +17,6 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Ecowave',
         home: const MyHomePage(),
-        onGenerateRoute: Routes.generateRoute,
         theme: ThemeData(
           scaffoldBackgroundColor: AppColors.white,
           useMaterial3: true,
