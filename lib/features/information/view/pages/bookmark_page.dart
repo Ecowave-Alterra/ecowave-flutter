4c02c2d533e30fd36c13334ef5fe71d8fc461b1e
FauziMaulia <125644172+FauziMaulia@users.noreply.github.com>
2023-06-23 21:40:08 +0700

    Features/fl 023 & migration to Api Register,Login,profile (#67)
    * Fixing Profile
    
    * Fixing Home
    
    * Fixing + edit password
    
    * FIxing Padding Profile
    
    * Change Single Scroll view to ListView
    
    * Edit_profile
    
    * fixing Home
    
    * Mencoba menerapkan Bloc pada state cek button status
    
    * bloc
    
    * Bloc Login
    
    * Fixing Eco Loading
    
    * Login
    
    * Hapus Macos
    
    * name Spesifick
    
    * fix
    
    * Register
    
    * Menyesuaikan di api Documenter untuk Login Dan Register
    
    * Fixing Overflow dan dispose
    
    * Profile
    
    * Harus Login dari Cart,Bookmart,Payment Detail
    
    * Migrasi Mock to Api Register, Login, Profile

--- lib/features/information/view/pages/bookmark_page.dart
+++ lib/features/information/view/pages/bookmark_page.dart
@@ -1,5 +1,7 @@
+import 'package:ecowave/features/home/bloc/home/home_bloc.dart';
 import 'package:ecowave/features/information/bloc/bookmark/bookmark_bloc.dart';
 import 'package:ecowave/features/information/view/widget/list_bookmart_widget.dart';
+import 'package:ecowave/features/profile/view/empty_session_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,54 +13,61 @@ class BookmarkPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Bookmark',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: AppFontWeight.regular,
-              color: AppColors.black,
-            ),
-          ),
-        ),
-        body: Column(
-          children: [
-            24.0.height,
-            Expanded(
-              child: BlocBuilder<BookmarkBloc, BookmarkState>(
-                builder: (context, state) {
-                  if (state is BookmarkLoading) {
-                    return const EcoLoading();
-                  } else if (state is BookmarkSuccess) {
-                    if (state.data.isEmpty) {
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 150.0),
-                        child: Center(
-                          child: EcoEmpty(
-                            massage: "Bookmark tidak tersedia",
-                            image: AppImages.emptyState,
-                            height: context.fullWidth / 2,
-                          ),
-                        ),
-                      );
-                    } else {
-                      return ListView.separated(
-                        itemBuilder: (context, index) =>
-                            ListBookmark(informationModel: state.data[index]),
-                        separatorBuilder: (context, index) => const Divider(
-                          height: 16,
-                        ),
-                        itemCount: state.data.length,
-                      );
-                    }
-                  } else {
-                    return const SizedBox.shrink();
-                  }
-                },
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state.token == ''){
+          return const EmptyUserPage();
+        }
+        return Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Bookmark',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: AppFontWeight.regular,
+                  color: AppColors.black,
+                ),
               ),
             ),
-          ],
-        ));
+            body: Column(
+              children: [
+                24.0.height,
+                Expanded(
+                  child: BlocBuilder<BookmarkBloc, BookmarkState>(
+                    builder: (context, state) {
+                      if (state is BookmarkLoading) {
+                        return const EcoLoading();
+                      } else if (state is BookmarkSuccess) {
+                        if (state.data.isEmpty) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 150.0),
+                            child: Center(
+                              child: EcoEmpty(
+                                massage: "Bookmark tidak tersedia",
+                                image: AppImages.emptyState,
+                                height: context.fullWidth / 2,
+                              ),
+                            ),
+                          );
+                        } else {
+                          return ListView.separated(
+                            itemBuilder: (context, index) => ListBookmark(
+                                informationModel: state.data[index]),
+                            separatorBuilder: (context, index) => const Divider(
+                              height: 16,
+                            ),
+                            itemCount: state.data.length,
+                          );
+                        }
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ));
+      },
+    );
   }
 }
