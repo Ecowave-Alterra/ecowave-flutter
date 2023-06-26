eaf01f060d492ca612210bcd61512e0a09ff4395
Kevin Natanael Wijaya <91646355+Kev9794@users.noreply.github.com>
2023-06-26 17:38:41 +0700

    [FL-033] Fix Bug on Detail Product (#74)
    * all integration api history pesanan
    
    * nambah CachedNetworkImage
    
    * fix button bug and color filter
    
    * fix overflow
    
    ---------
    
    Co-authored-by: wahyu restu pamuji <wahyurestup1@gmail.com>

--- lib/features/ecommerce/view/widgets/card_product_widget.dart
+++ lib/features/ecommerce/view/widgets/card_product_widget.dart
@@ -61,7 +61,7 @@ class CardProduct extends StatelessWidget {
                                 child: ColorFiltered(
                                   colorFilter: ColorFilter.mode(
                                       product.stock == 0
-                                          ? Colors.black
+                                          ? Colors.grey
                                           : Colors.transparent,
                                       BlendMode.saturation),
                                   child: Image.network(
