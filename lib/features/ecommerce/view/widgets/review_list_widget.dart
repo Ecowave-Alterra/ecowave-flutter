a65c34be081fa42b9de5b88763dc0fa9aff8bead
Kevin Natanael Wijaya <91646355+Kev9794@users.noreply.github.com>
2023-06-28 20:58:27 +0700

    [FL-033] fix bug filter and review (#79)
    * fix bug filter
    
    * fix review scroll

--- lib/features/ecommerce/view/widgets/review_list_widget.dart
+++ lib/features/ecommerce/view/widgets/review_list_widget.dart
@@ -21,6 +21,7 @@ class _ReviewListWidgetState extends State<ReviewListWidget> {
         (index) => widget.review.review![index]);
     return ListView.builder(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       itemCount: reviews.length,
       itemBuilder: (context, index) {
         return Column(
