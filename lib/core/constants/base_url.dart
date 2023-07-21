ea399ce2665a046d0208c9be2dc0df7b7b8f85c7
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-07-21 07:53:20 +0700

    migrate url api & version app

--- lib/core/constants/base_url.dart
+++ lib/core/constants/base_url.dart
@@ -3,5 +3,5 @@ part of 'package:ecowave/core.dart';
 class BaseURL {
   static const String mock =
       "https://607fe676-8a2a-4e6c-89cb-9c9a54db7532.mock.pstmn.io/";
-  static const String api = "https://ecowave-h64wmjjkza-uc.a.run.app/";
+  static const String api = "https://ecowave-4gk3tp7c3a-uc.a.run.app/";
 }
